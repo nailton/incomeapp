6f0234b92fad444450415feeb139aa8abfeda7d4
Nailton Sousa <nailton.osn@gmail.com>
2024-02-13 22:42:34 -0300

    - calculate total income when clicked button - show results

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,26 @@
 slint::include_modules!();
 
+const TAXPER: f64 = 0.30;
+const OWNERPER: f64 = 0.55;
+const PROFIT: f64 = 0.05;
+const OPERPER: f64 = 0.10;
+
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
 
-    // ui.on_request_increase_value({
-    //     let ui_handle = ui.as_weak();
-    //     // move || {
-    //     //         let ui = ui_handle.unwrap();
-    //     //         ui.set_counter(ui.get_counter() + 1);
-    //     // }
-    // });
+    ui.on_divide_income({
+        let ui_handle = ui.as_weak();
+        move | string | {
+            let ui = ui_handle.unwrap();
+            let num:f64 = string.trim().parse().unwrap();
+            let tax:f64 = num * TAXPER;
+            let owner:f64 = num * OWNERPER;
+            let profit:f64 = num * PROFIT;
+            let oper:f64 = num * OPERPER;
+            let result: String = format!("Taxes: {:.2}\nOwner: {:.2}\nProfit: {:.2}\nOpEx: {:.2}", {tax}, {owner}, {profit}, {oper});
+            ui.set_results(result.into());
+        }
+    });
 
     ui.run()
 }
